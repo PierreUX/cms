7c0d7905b7f1623b4d1767c0950c1cf934551581
bartonicek <pierre.baptiste.bartonicek@gmail.com>
2016-05-30 22:50:53 +0200

    ajout de la config

---
+++ config/constant.php
@@ -0,0 +1,4 @@
+<?php
+
+global const ROOT = $_SERVER['HTTP_HOST'];
+global const URI = $_SERVER['REQUEST_URI'];
---
+++ config/rooter.php
@@ -0,0 +1,10 @@
+<?php
+$url = $_GET[''];
+switch($url){
+	case '/':
+		$page = "/controller/index.php";
+		break;
+	case '/maPage':
+		$page = "/controller/maPage.php";
+		break;
+}
